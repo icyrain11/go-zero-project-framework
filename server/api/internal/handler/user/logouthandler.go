86bcd2c82a380b1365c401cdff336741a3f905aa
icyrain11 <734931302@qq.com>
2023-12-12 20:45:48 +0800

    feat: modify project struct

--- server/api/internal/handler/user/logouthandler.go
+++ server/api/internal/handler/user/logouthandler.go
@@ -1,22 +1,20 @@
 package user
 
 import (
-	"net/http"
-	"github.com/zeromicro/go-zero/rest/httpx"
-	"xhttp "github.com/zeromicro/x/http""
+	xhttp "github.com/zeromicro/x/http"
 	"go-zero-demo/server/api/internal/logic/user"
 	"go-zero-demo/server/api/internal/svc"
-
+	"net/http"
 )
 
 func LogoutHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		l := user.NewLogoutLogic(r.Context(), svcCtx)
 		err := l.Logout()
-        if err != nil {
+		if err != nil {
 			xhttp.JsonBaseResponseCtx(r.Context(), w, err)
 		} else {
-			xhttp.JsonBaseResponseCtx(r.Context(), w, resp)
+			xhttp.JsonBaseResponseCtx(r.Context(), w, nil)
 		}
 	}
 }
